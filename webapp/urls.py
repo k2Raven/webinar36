36c05f6981ae77da26fac4dfa535f2e31f82b8f8
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2023-11-22 20:25:18 +0600

    полностью реализовал представление index_view

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webapp.views import index_view
+from webapp.views import index_view, cat_stats_view
 
 urlpatterns = [
     path('', index_view),
+    path('cat_stats/', cat_stats_view),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from webapp.cat import Cat
+from django.http import HttpResponseRedirect
 
 
 def index_view(request):
     if request.method == "GET":
         return render(request, 'index.html')
+    elif request.method == "POST":
+        Cat.name = request.POST.get('cat_name')
+        return HttpResponseRedirect('/cat_stats')
 
 
 def cat_stats_view(request):
-    print('hello')
-    2 != 0
-    print('test')
+    if request.method == "GET":
+        return render(request, 'cat_stats.html')
